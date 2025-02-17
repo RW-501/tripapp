a790e788eca3cfcafa1b6d990191cfaf0e3a8977
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-02-16 20:15:35 -0600

    kn

--- scripts/js/auth.js
+++ scripts/js/auth.js
@@ -1191,11 +1191,13 @@ window.showUserSignedOutMessage = showUserSignedOutMessage;
 // Export Firestore, Storage, and Auth instances for use in other modules
 export {
     db, doc, getDoc, updateDoc,
-    auth, analytics, deleteDoc,  userId, 
-getStorage, ref, uploadBytes, getDownloadURL, limit, arrayUnion, RecaptchaVerifier, increment, getDoc, arrayRemove, signInWithPhoneNumber,
-query, updateDoc, setDoc, addDoc, signInAnonymously, orderBy, onAuthStateChanged,
-uploadBytesResumable, signInWithPopup, FacebookAuthProvider, GoogleAuthProvider, startAfter,
-OAuthProvider, signOut, deleteDoc, getFirestore, serverTimestamp,  
+    auth, deleteDoc,  userId, getStorage, ref, uploadBytes, 
+    getDownloadURL, limit, arrayUnion, RecaptchaVerifier, increment,
+     arrayRemove, signInWithPhoneNumber, query, setDoc, 
+     addDoc, signInAnonymously, orderBy, onAuthStateChanged, 
+     uploadBytesResumable, signInWithPopup, FacebookAuthProvider, 
+     GoogleAuthProvider, startAfter, OAuthProvider, signOut, deleteDoc,
+      getFirestore, serverTimestamp,  
 createUserWithEmailAndPassword, signInWithEmailAndPassword, deleteObject,
 where, getDocs, storage, getAuth, collection, analytics,EmailAuthProvider,
 googleProvider,onSnapshot ,writeBatch ,batch, linkWithCredential,
